e41a733aa8648c05ac49f168616cc5fe4893342c
KnuxV <81012476+KnuxV@users.noreply.github.com>
2023-11-13 15:04:28 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,49 +1,12 @@
-// create web server with node.js
-// run server with: node comment.js
-// then visit http://localhost:8000 to see the result
-// use ctrl+c to stop the server
-// use curl -d "user=abc&comment=def" http://localhost:8000 to send a post request
-// use curl http://localhost:8000 to send a get request
-// use curl -X DELETE http://localhost:8000 to send a delete request
+// create web server
+const http = require('http');
 
-var http = require('http');
-var url = require('url');
-var items = [];
-
-var server = http.createServer(function(req, res) {
-    switch (req.method) {
-        case 'POST':
-            var item = '';
-            req.setEncoding('utf8');
-            req.on('data', function(chunk) {
-                item += chunk;
-            });
-            req.on('end', function() {
-                items.push(item);
-                res.end('OK\n');
-            });
-            break;
-        case 'GET':
-            items.forEach(function(item, i) {
-                res.write(i + ') ' + item + '\n');
-            });
-            res.end();
-            break;
-        case 'DELETE':
-            var path = url.parse(req.url).pathname;
-            var i = parseInt(path.slice(1), 10);
-            if (isNaN(i)) {
-                res.statusCode = 400;
-                res.end('Invalid item id');
-            } else if (!items[i]) {
-                res.statusCode = 404;
-                res.end('Item not found');
-            } else {
-                items.splice(i, 1);
-                res.end('OK\n');
-            }
-            break;
-    }
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
 });
 
-server.listen(8000);
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
